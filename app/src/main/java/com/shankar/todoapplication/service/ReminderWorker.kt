3cd410a35efba8f8f3805cc03544c351b5670922
shankar <harryshankar@outlook.com>
2023-10-01 18:01:04 +0545

    adds condition for skd in notification builder

--- app/src/main/java/com/shankar/todoapplication/service/ReminderWorker.kt
+++ app/src/main/java/com/shankar/todoapplication/service/ReminderWorker.kt
@@ -1,9 +1,11 @@
 package com.shankar.todoapplication.service
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.PowerManager
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
@@ -45,6 +47,9 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) :
         // Build the notification
         val notificationIntent = Intent(applicationContext, TodoActivity::class.java)
         notificationIntent.putExtra("KEY_ROWID", rowId)
+        // Show the notification
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val pendingIntent = PendingIntent.getActivity(
             applicationContext,
@@ -53,6 +58,15 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) :
             PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
         )
         val CHANNEL_ID = "1.0"
+        // Notification channel is required for Android Oreo and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "reminder_channel_id",
+                "Reminder Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
         val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
 //             .setSmallIcon(R.drawable.ic_notification)
 //             .setContentTitle(applicationContext.getString(R.string.notify_new_task_title))
@@ -62,9 +76,7 @@ class ReminderWorker(context: Context, workerParams: WorkerParameters) :
             .setAutoCancel(true)
             .build()
 
-        // Show the notification
-        val notificationManager =
-            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         notificationManager.notify(rowId.toInt(), notification)
     }
 }
